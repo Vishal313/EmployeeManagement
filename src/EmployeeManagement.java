8ce49088c3b22b4d97859cfd8f473a684f034fd5
Vishal313 <palaivishal@gmail.com>
2021-06-03 12:45:48 +0530

    Validate User and Login

---
+++ src/EmployeeManagement.java
@@ -0,0 +1,15 @@
+
+public class EmployeeManagement {
+
+	public static void main(String[] args) {
+		
+		ValidateUser v1 = new ValidateUser();
+		String empLevel = ValidateUser.empLevel;
+		System.out.println(empLevel);
+		
+//		switch(empLevel) {
+//			
+//		}
+	}
+
+}
---
+++ src/ValidateUser.java
@@ -0,0 +1,84 @@
+import java.io.File;
+import java.util.Scanner;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class ValidateUser {
+	private static Scanner x;
+	public static String empLevel = "";
+	
+	public ValidateUser() {
+		Scanner sc = new Scanner(System.in);
+		
+		boolean login = true;
+		String username = "";
+		String password = "";
+		String filepath = "data/up.txt";
+		
+		while (login) {
+			System.out.print("Enter Your UserName: ");
+			username = sc.nextLine();
+			System.out.print("Enter Your Password: ");
+			password = sc.nextLine();
+			password = getMd5(password);
+			login = verifyLogin(username, password, filepath);
+		}
+		sc.close();
+	}
+	
+	public static boolean verifyLogin(String username, String password, String filepath) {
+		boolean found = false;
+		String tempUsername = "";
+		String tempPassword = "";
+		String role = "";
+		try {
+			x = new Scanner(new File(filepath));
+			x.useDelimiter("[,\n]");
+			
+			while (x.hasNext() && !found) {
+				tempUsername = x.next();
+				tempPassword = x.next();
+				role = x.next();
+				
+				if (tempUsername.trim().equals(username.trim()) && tempPassword.trim().equals(password.trim())) {
+					found = true;
+					empLevel = role;
+				}
+			}
+			x.close();
+			if (found)
+				System.out.println("You are Logged in Successfully");
+			else {
+				System.out.println("Login Failed");
+				System.out.println("-------------------------------");
+			}
+			
+		} 
+		catch (Exception e) {
+			System.out.println("Error in Data Store");
+		}
+		
+		return !found;
+	}
+	
+	public static String getMd5(String input) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			  
+	        byte[] messageDigest = md.digest(input.getBytes());
+	
+	        BigInteger no = new BigInteger(1, messageDigest);
+	
+	        String hashtext = no.toString(16);
+	        while (hashtext.length() < 32) {
+	            hashtext = "0" + hashtext;
+	        }
+	        return hashtext;
+		}
+		catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+		}
+	}
+
+}
